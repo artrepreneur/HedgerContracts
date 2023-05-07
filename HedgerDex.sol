3b2e3409bdc8c73f7591c4eb9cdf1309170bcae0
Artrepreneur1 <46770824+artrepreneur@users.noreply.github.com>
2023-05-07 16:36:51 -0400

    Update HedgerDex.sol

--- HedgerDex.sol
+++ HedgerDex.sol
@@ -1,20 +1,24 @@
+
+// SPDX-License-Identifier: MIT
+/*
+* This is a solidity smart contract for a hedge fund. The contract uses the OpenZeppelin library to import ERC20 and AccessControl contracts, and also interfaces with Chainlink and UniswapV2Router02 for token swaps. The fund manager can add non-pool tokens and their corresponding price feeds to the contract, and users can deposit and withdraw USDT to and from the pool. The contract calculates the net asset value of the pool using Chainlink price feeds and the balances of non-pool tokens, and uses 1inch to swap non-pool tokens to USDT. The fund manager can also swap tokens using UniswapV2Router02. The contract also has role-based access control, with a DEFAULT_ADMIN_ROLE and a FUND_MANAGER_ROLE.
+* Note that multichain portfolios are only possible by moving underlying liqduity through a
+* stablecoin brigde, that is until teleport has the liquidity it needs.
+*/
+
+
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/access/AccessControl.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "./FundBase.sol";
-import "./IOneSplitAudit.sol"; // Make sure to import the IOneSplitAudit interface
-
 
 
 contract HedgerDex is FundBase, ERC20("HedgerDex Token", "HDT"), AccessControl {
 
     bytes32 public constant FUND_MANAGER_ROLE = keccak256("FUND_MANAGER_ROLE");
     uint256 public lockUpDuration;
-    mapping(address => uint256) public balances;
     uint256 public totalBalance;
     uint256 public totalShares;
-    mapping(address => uint256) public shareBalances;
     IERC20 public stablecoin;
 
     event Deposit(address indexed sender, uint256 usdtAmount, uint256 poolTokensMinted, uint256 lockUpPeriodEnd);
@@ -28,9 +32,16 @@ contract HedgerDex is FundBase, ERC20("HedgerDex Token", "HDT"), AccessControl {
     address public _stablecoin = address(0x3E7d1eAB13ad0104d2750B8863b489D65364e32D);
     IOneSplitAudit oneInchRouter = IOneSplitAudit(ONEINCH_ROUTER);
 
-    // Definition for deposits
-    mapping(address => uint256) deposits;
-    mapping(address => uint256) lockUpPeriods;
+    struct UserInfo {
+        uint256 deposit;
+        uint256 lockUpPeriod;
+        uint256 balance;
+        uint256 shareBalance;
+        uint256 depositedAt;
+    }
+
+    mapping(address => UserInfo) public userInfos;
+
 
     constructor() {
         _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
@@ -44,32 +55,31 @@ contract HedgerDex is FundBase, ERC20("HedgerDex Token", "HDT"), AccessControl {
     }
 
  
-
-
     modifier onlyAdmin() {
         require(hasRole(DEFAULT_ADMIN_ROLE, msg.sender), "Ownable");
         _;
     }
     
-    mapping(address => uint256) public depositedAt; 
 
     
     function addLiquidity(uint256 _usdtAmount) public {
         require(_usdtAmount > 0, "Amount must be greater than zero");
-        uint256 nav = getNav();//getNav();
+        uint256 nav = getNav();
         uint256 poolTokensToMint = (_usdtAmount * totalShares) / nav;
         uint256 fee = (_usdtAmount * 2) / 100;
         SafeERC20.safeTransferFrom(stablecoin, msg.sender, address(this), _usdtAmount);
         SafeERC20.safeTransfer(stablecoin, fundManagementWallet, fee);
-        balances[msg.sender] += (_usdtAmount - fee);
+        userInfos[msg.sender].balance += (_usdtAmount - fee);
         totalBalance += (_usdtAmount - fee);
-        shareBalances[msg.sender] += poolTokensToMint;
+        userInfos[msg.sender].shareBalance += poolTokensToMint;
         totalShares += poolTokensToMint;
-        depositedAt[msg.sender] = block.timestamp;
+        userInfos[msg.sender].depositedAt = block.timestamp;
         uint256 lockUpPeriodEnd = block.timestamp + lockUpDuration;
-        lockUpPeriods[msg.sender] = lockUpPeriodEnd;
+        userInfos[msg.sender].lockUpPeriod = lockUpPeriodEnd;
         emit Deposit(msg.sender, (_usdtAmount - fee), poolTokensToMint, lockUpPeriodEnd);
     }
+
+
 
     function removeLiquidity(uint256 _shareAmount) public {
         require(_shareAmount > 0, "Amount must be greater than zero");
@@ -83,29 +93,29 @@ contract HedgerDex is FundBase, ERC20("HedgerDex Token", "HDT"), AccessControl {
         }
 
         uint256 usdtAmount = (stablecoin.balanceOf(address(this)) * _shareAmount) / totalShares;
-        bool canWithdraw = (block.timestamp >= lockUpPeriods[msg.sender]);
+        bool canWithdraw = (block.timestamp >= userInfos[msg.sender].lockUpPeriod);
         uint256 fee = 0;
         if (!canWithdraw) {
             fee = (usdtAmount * 5) / 100;
         }
 
-        balances[msg.sender] -= (usdtAmount);
+        userInfos[msg.sender].balance -= (usdtAmount);
         totalBalance -= (usdtAmount);
-        uint256 deposit = deposits[msg.sender];
-        uint256 shareBalance = shareBalances[msg.sender];
+        uint256 deposit = userInfos[msg.sender].deposit;
+        uint256 shareBalance = userInfos[msg.sender].shareBalance;
         uint256 initialDeposit = (deposit *  _shareAmount) / shareBalance;
         uint256 profit = usdtAmount - initialDeposit;
         uint256 fee20 = 0;
         fee20 = (profit * 20) / 100;
         fee += fee20;
-        shareBalances[msg.sender] -= _shareAmount;
+        userInfos[msg.sender].shareBalance -= _shareAmount;
         totalShares -= _shareAmount;
         SafeERC20.safeTransfer(stablecoin, msg.sender, (usdtAmount - fee));
         SafeERC20.safeTransfer(stablecoin, fundManagementWallet, fee);
         emit Withdrawal(msg.sender, (usdtAmount - fee), _shareAmount, fee);
     }
 
-     function swapTo1inch(address _fromToken, address _toToken, uint256 _amountIn, uint256 _amountOutMin, uint256 _maxPriceImpact) internal onlyRole(FUND_MANAGER_ROLE) {
+    function swapTo1inch(address _fromToken, address _toToken, uint256 _amountIn, uint256 _amountOutMin, uint256 _maxPriceImpact) internal onlyRole(FUND_MANAGER_ROLE) {
         SafeERC20.safeApprove(IERC20(_fromToken), address(ONEINCH_ROUTER), _amountIn);
         if (!isNonPoolToken(_fromToken)) {
             nonPoolTokens.push(_fromToken);
@@ -121,7 +131,6 @@ contract HedgerDex is FundBase, ERC20("HedgerDex Token", "HDT"), AccessControl {
         emit TokenSwapped(_fromToken, _amountIn, _toToken, amountOut);
     }
     
-
     function setLockUpDuration(uint256 _duration) public onlyAdmin {
         lockUpDuration = _duration;
     }
@@ -141,5 +150,11 @@ contract HedgerDex is FundBase, ERC20("HedgerDex Token", "HDT"), AccessControl {
     function addNonPoolToken(address _token) external onlyRole(FUND_MANAGER_ROLE) {
         _addNonPoolToken(_token);
     }
+    
+
+
 
 }
+
+
+
